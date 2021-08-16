b1f6aa95564b6d77532a1a529eb3ac2ad23e4567
robonexx <robertwagar@gmail.com>
2021-08-16 10:16:20 +0200

    pushing up what ive done this morning

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
+import Navbar from './components/layout/Navbar'
+import Users from './components/users/Users';
 
-function App() {
-  return (
+class App extends Component{
+
+  state = {
+    users: [],
+    loading: false
+  }
+
+  async componentDidMount() {
+    this.setState({ loading: true })
+    const res = await axios
+      .get('https://api.github.com/users')
+    
+      this.setState({ users: res.data, loading: false})
+    console.log(res.data)
+  }
+
+  render() {
+
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Navbar />
+        <div className="container">
+          <Users loading={this.state.loading} users={this.state.users}/>
+        </div>
+       
     </div>
   );
+  }
 }
 
 export default App;
