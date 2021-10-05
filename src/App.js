d7b209bb897cbd3426ea50638f91a2ea4ac8b8c1
robonexx <robertwagar@gmail.com>
2021-10-05 07:58:10 +0200

    moved search and users components to home component and made a not found page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './components/pages/Home';
 import About from './components/pages/About'
+import NotFound from './components/pages/NotFound';
 import Navbar from './components/layout/Navbar'
-import Users from './components/users/Users';
 import User from './components/users/User'
-import Search from './components/users/Search'
 import Alert from './components/layout/Alert'
-
 import GithubState from './context/github/GithubState';
 import AlertState from './context/alert/AlertState';
 
@@ -23,15 +22,10 @@ const App = () => {
         <div className="container">
             <Alert />
             <Switch>
-              <Route exact path="/" render={props => (
-                <Fragment>
-            <Search />
-          <Users />
-                </Fragment>
-              )} />
-              <Route exact path='/about' component={About} />
-              
-              <Route exact path="/user/:login" component={User}/>
+                <Route exact path="/" component={Home}/>
+                <Route exact path='/about' component={About} />
+                <Route exact path="/user/:login" component={User} />
+                <Route component={NotFound} />
             </Switch>
        
         </div>
